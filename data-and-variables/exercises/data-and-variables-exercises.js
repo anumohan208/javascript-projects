cb4b170049d71e0c247b1d94a95f8c5d6a512065
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-01-11 16:21:17 -0600

    Class2_Exercise

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,4 +1,4 @@
-// Declare and assign the variables below
+/*// Declare and assign the variables below
 let nameOfShuttle = 'Determination';
 let speedOfShuttle = 17500;
 let distanceToMars = 225000000;
@@ -23,3 +23,42 @@ let hoursToMoon= milesToMoon/speedOfShuttle;
 let daysToMoon= hoursToMoon/24;
 // Print the results of the trip to the moon below
 console.log(nameOfShuttle,"will take",daysToMoon,"days to reach Moon")
+*/
+let score=0;
+const input = require('readline-sync');
+
+let name = input.question("Enter your name: ");
+
+let question1 =input.question("What is the capital of France?");
+let answerToQuestion1="france";
+if(question1.toLowerCase==answerToQuestion1.toLowerCase){
+    score = score+1;
+    console.log("Congratulations",name,"Correct Answer!")
+}
+let question2 =input.question("Which planet is known as the Red Planet?");
+let answerToQuestion2="Mars";
+
+if(question2.toLowerCase==answerToQuestion2.toLowerCase){
+    score = score+1;
+}
+
+let question3 =input.question("What is the largest mammal on Earth?");
+let answerToQuestion3="Blue Whale";
+
+if(question3.toLowerCase==answerToQuestion3.toLowerCase){
+    score = score+1;
+}
+
+let question4 =input.question("What is the largest planet in our solar system?");
+let answerToQuestion4="Jupiter";
+
+if(question4.toLowerCase==answerToQuestion4.toLowerCase){
+    score = score+1;
+}
+
+let question5 =input.question("What is the currency of Germany?");
+let answerToQuestion5="Euro";
+
+if(question5.toLowerCase==answerToQuestion5.toLowerCase){
+    score = score+1;
+}
