4b84f58213f586cac76fbf6cc8e94d3c09641935
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-02-29 13:01:03 -0600

    Class15 Exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,18 @@
 //Code Your Solution Below
+function init() {
+
+    const button = document.getElementById("submit");
+    const testName =document.getElementsByName("testName")[0];
+
+    button.addEventListener('click', event => {
+    
+        if(testName.value ==='')
+           alert("Testname required");
+      
+        
+    });
+
+
+}
+
+window.addEventListener("load", init);
