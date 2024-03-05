715784edadc41efd47bb3b911f76c892270da420
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-03-04 22:01:40 -0600

    Class16 Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,29 @@
 //TODO: Add Your Code Below
+window.addEventListener("load",function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+    response.json().then(function(json) {
+    for(let i=0;i<json.length;i++){
+        let astronautDiv = document.createElement("div");
+        astronautDiv.classList.add("astronaut");
+        if(json[i].active){
+         json[i].active ='<span style="color: green;">true</span>';
+        }
+        astronautDiv.innerHTML = `
+            <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li>Active: ${json[i].active}</li>
+                    <li>Skills: ${json[i].skills.join(', ')}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}" >
+        `;
+    document.getElementById("container").appendChild(astronautDiv);
+
+    }
+
+    });
+    });
+
+});
